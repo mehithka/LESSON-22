1c3e9a3ca86f9c2afd7b06141345c8bb92b72048
mehithka <mehith.karacasia@gmail.com>
2025-02-12 15:23:40 +0500

    a2

---
+++ a2.py
@@ -0,0 +1,11 @@
+setx = {'green','blue'}
+sety={'blue','yellow'}
+print('original set elements: ')
+print(setx)
+print(sety)
+print('\n Intersection of two said sets:')
+setz = setx.intersection(sety)
+print(setz)
+
+setz2=setx.union(sety)
+print(setz2)
