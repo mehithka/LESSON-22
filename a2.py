81eefafeec4da6c319921876090693c8596e6b27
mehithka <mehith.karacasia@gmail.com>
2025-02-12 15:39:13 +0500

    HW

--- a2.py
+++ a2.py
@@ -9,3 +9,15 @@ print(setz)
 
 setz2=setx.union(sety)
 print(setz2)
+
+setz3=setx.difference(sety)
+print(setz3)
+
+setz4=sety.difference(setx)
+print(setz4)
+
+setz5= setx.symmetric_difference(setx)
+print(setz5)
+
+setz6 = sety.symmetric_difference(setx)
+print(setz6)
